bff7f8fe8899779adba525949eff4277f4a2a70b
konson-is <i.shoji.0731@gmail.com>
2024-04-06 23:00:47 +0900

    Added hello lambda and esbuild

--- lib/LambdaStack.ts
+++ lib/LambdaStack.ts
@@ -1,5 +1,8 @@
 import { Stack, StackProps, Stage, StageProps } from "aws-cdk-lib";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
+import { join } from "path";
 
 interface LambdaStackProps extends StackProps {
   stageName?: string;
@@ -8,5 +11,13 @@ interface LambdaStackProps extends StackProps {
 export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
+    new NodejsFunction(this, "hello-lambda", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "handler",
+      entry: join(__dirname, "..", "services", "hello.ts"),
+      environment: {
+        STAGE: props.stageName!,
+      },
+    });
   }
 }
